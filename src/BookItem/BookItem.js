0af3da93d7e945e6dfbcc7c2a629dd816e8d725d
klguenth <klguenth@gmail.com>
2019-08-25 15:22:55 -0500

    updated functionality in BookList and BookSearch

--- src/BookItem/BookItem.js
+++ src/BookItem/BookItem.js
@@ -13,48 +13,13 @@ export default class BookItem extends React.Component {
     };
   }
 
-  titleChanged(title) {
-    this.setState({
-      title
-    });
-  }
-
-  authorsChanged(authors) {
-    this.setState({
-      authors
-    });
-  }
-
-  priceChanged(price) {
-    this.setState({
-      price
-    });
-  }
-
-  descriptionChanged(description) {
-    this.setState({
-      description
-    });
-  }
-
-  urlChanged(url) {
-    this.setState({
-      url
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    const bookInfo = (({title, authors, isEbook, description}) => ({title, authors, isEbook, description}))(this.state);
-  }
-
   render() {
     return (
       <div className="bookItem">
-          <h3>Title: {onChange={e => this.titleChanged(e.target.value)}</h3>
-          <h4>Author: {onChange={e => this.authorsChanged(e.target.value)}}</h4>
-          <p>{onChange={e => this.priceChanged(e.target.value)}}</p>
-          <p>{onChange={e => this.descriptionChanged(e.target.value)}}</p>
+          <h3>Title: {this.state.title}</h3>
+          <h4>Author: {this.state.authors}</h4>
+          <p>{this.state.price}</p>
+          <p>{this.state.description}</p>
           {/*<img src={this.imageLinks.smallThumbnail} alt="Book Thumbnail"></img>*/}
       </div>
     )
--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -3,19 +3,32 @@ import './BookList.css';
 import BookItem from '../BookItem/BookItem.js';
 
 class BookList extends Component {
-  render() {  
-    return (
-      <div className="bookList">
-        <form className="formList">
-          <ul>
-            <li><BookItem /></li>
-            <li><BookItem /></li>
-            <li><BookItem /></li>
-            <li><BookItem /></li>
-          </ul>
-        </form>
-      </div>
-    );
+  bookSearchResults(responseJson) {
+    $('#bookItem').empty();
+    for (let i = 0; i < responseJson.results.length; i++) {
+      $('#bookItem').append(
+        `<li>
+          <h3>${responseJson.results[i].title}</h3>
+          <h4>${responseJson.results[i].authors}</h4>
+          <p>${responseJson.results[i].price}</p>
+          <p>${responseJson.results[i].description}</p>
+        </li>`
+        );
+      }
   }
+  render() {
+      return (
+        <div className="bookList">
+          <form className="formList">
+            <ul>
+              <li><BookItem /></li>
+              <li><BookItem /></li>
+              <li><BookItem /></li>
+              <li><BookItem /></li>
+            </ul>
+          </form>
+        </div>
+      );
+    }
 }
 export default BookList;
