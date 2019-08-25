331e664a01ecb131aa8420db2bbdda5fb25c2757
klguenth <klguenth@gmail.com>
2019-08-25 12:47:05 -0500

    updated functionality of state

--- src/BookItem/BookItem.js
+++ src/BookItem/BookItem.js
@@ -43,13 +43,18 @@ export default class BookItem extends React.Component {
     });
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    const bookInfo = (({title, authors, isEbook, description}) => ({title, authors, isEbook, description}))(this.state);
+  }
+
   render() {
     return (
       <div className="bookItem">
-          <h3>Title: {this.state.title}</h3>
-          <h4>Author: {this.state.authors}</h4>
-          <p>{this.state.isEbook}</p>
-          <p>{this.state.description}</p>
+          <h3>Title: onChange={e => this.titleChanged(e.target.value)}</h3>
+          <h4>Author: onChange={e => this.authorsChanged(e.target.value)}</h4>
+          <p>onChange={e => this.priceChanged(e.target.value)}</p>
+          <p>onChange={e => this.descriptionChanged(e.target.value)}</p>
           {/*<img src={this.imageLinks.smallThumbnail} alt="Book Thumbnail"></img>*/}
       </div>
     )
